01164260ed0bc3950a4e4ee15090b34173976cce
prateekgehsaan <156509081+prateekgehsaan@users.noreply.github.com>
2025-03-11 17:45:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ let grandTotal = 0;
 dates.forEach(date => {
     const dateExpenses = expenses.filter(exp => exp.date === date);
     let dailyTotal = 0;
-    tbody += `<tr data-date="${date}" onclick="getTableDate('${date}')" ><td>${date}</td>`; // Add data-date attribute for easier filtering
+    tbody += `<tr data-date="${date}"><td>${date}</td>`; // Add data-date attribute for easier filtering
     categories.forEach(cat => {
         const catExpenses = dateExpenses.filter(exp => exp.category === cat);
         const amounts = catExpenses.map(exp => 
@@ -433,10 +433,9 @@ $('#downloadBtn').on('click', function() {
     ExportToExcel();
 });
 
-
-function getTableDate(date) {
-	debugger;
-    document.getElementById("date").value = date; 
-}
-
+ $(document).on("click", "#expenseTable td[data-date]", function() {
+    debugger;
+    let selectedDate = $(this).data("date");
+    $("#date").val(selectedDate);
+});
  
